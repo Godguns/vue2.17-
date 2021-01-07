a128c75bbe42899a793144b2358339256fc18247
jinlang.dong@jinlang.dong <jinlang.dong@ly.com>
2021-01-07 14:22:46 +0800

    注释

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-05 15:06:19
+ * @LastEditTime: 2021-01-07 14:22:39
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-2.1.7\src\compiler\index.js
+ */
 /* @flow */
 
 import { parse } from './parser/index'
@@ -6,6 +14,7 @@ import { generate } from './codegen/index'
 
 /**
  * Compile a template.
+ * 编译
  */
 export function compile (
   template: string,
